59f3357f8781902f4fa74194c56c812628fd6959
hamid reza <126154017+hamidreza1983@users.noreply.github.com>
2024-10-17 10:45:59 +0330

    create model

--- root/admin.py
+++ root/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Agents, Ability
+
+admin.site.register(Agents)
+admin.site.register(Ability)
 
 # Register your models here.
--- root/models.py
+++ root/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Agents(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ability = models.ForeignKey(Ability, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="agent", default="default.jpg")
+    twitter = models.CharField(max_length=220)
+    instagram = models.CharField(max_length=220)
+    facebook = models.CharField(max_length=220)
+    linkedin = models.CharField(max_length=220)
+    status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.user.first_name
