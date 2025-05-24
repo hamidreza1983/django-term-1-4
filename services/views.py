994b7b3c110519a968462df256da0570831f829a
hamidreza1983 <hamidrezacoder1983@gmail.com>
2025-05-24 23:35:36 +0200

    edit service view

--- services/views.py
+++ services/views.py
@@ -4,69 +4,69 @@ from django.core.paginator import Paginator
 from django.views.generic import ListView, DetailView, TemplateView
 from django.http import HttpResponseBadRequest
 
-class ServicesView(TemplateView):
-    template_name = "services/services-api.html"
-
-
-# class ServicesView(ListView):
-#     model = Services
+# class ServicesView(TemplateView):
 #     template_name = "services/services.html"
-#     context_object_name = "services"
-#     paginate_by = 5
 
-#     # queryset = Services.objects.filter(status=True)#
-#     def get_queryset(self):
-#         if self.kwargs.get("category"):
-#             services = self.model.objects.filter(
-#                 category__title=self.kwargs.get("category"), status=True
-#             )
-#         elif self.kwargs.get("name"):
-#             services = Services.objects.filter(
-#                 creator__user__email=self.kwargs.get("name"), status=True
-#             )
-#         elif self.request.GET.get("search"):
-#             search = self.request.GET.get("search")
-#             services = self.model.objects.filter(
-#                 content__contains=search, status=True
-#             )
-#         else:
-#             services = Services.objects.filter(status=True)  #
-#         return services
 
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         first = 1
-#         service_paginate = Paginator(self.get_queryset(), 1)
-#         last = service_paginate.num_pages
-#         context["first"] = first
-#         context["last"] = last
-#         return context
+class ServicesView(ListView):
+    model = Services
+    template_name = "services/services.html"
+    context_object_name = "services"
+    paginate_by = 5
 
-#     def post(self, request, *args, **kwargs):
-#         #       # برای خالی کردن سبد از متود زیر استفاده میشود
-#         #       del request.session["cart"]
-#         #       request.session.modified = True
-#         # دیکشنری کارت از شژن فراخوانی میشود و اگر نباشد با یک دیکشنری خالی مقدار دهی میشود
-#         cart = request.session.get("cart", {})
-#         # آی دی محصود و تعدادش از رکوئست پست فراخوانی میشود
-#         # علت استفاده از بلاک زی این است که ممکن است گاهی دیتای دیگری در رکوئست پست ارسال شود
-#         # لذا برای اینکه سرور ارور ندهد با بلاک زیر بررسی میشود
-#         try:
-#             id = request.POST.get("product_id", "0")
-#             quantity = request.POST.get("quantity", "1")
-#         except ValueError:
-#             return HttpResponseBadRequest("Invalid input")
-#         # اگر آی دی محصول در سبد بود به مقدار قبلی اش اضافه میشود
-#         # اگر نباشد آی دی به همراه مقدارش در کارت قرار میگیرد
-#         if id in cart:
-#             cart[id] = str(int(cart[id]) + int(quantity))
-#         else:
-#             cart[id] = quantity
-#         # برای اطمینان بیشتر مجدد کارت در سژن ست میشود
-#         request.session["cart"] = cart
-#         # تغییرات سژن اعمال میشود
-#         request.session.modified = True
-#         return redirect(request.path_info)
+    # queryset = Services.objects.filter(status=True)#
+    def get_queryset(self):
+        if self.kwargs.get("category"):
+            services = self.model.objects.filter(
+                category__title=self.kwargs.get("category"), status=True
+            )
+        elif self.kwargs.get("name"):
+            services = Services.objects.filter(
+                creator__user__email=self.kwargs.get("name"), status=True
+            )
+        elif self.request.GET.get("search"):
+            search = self.request.GET.get("search")
+            services = self.model.objects.filter(
+                content__contains=search, status=True
+            )
+        else:
+            services = Services.objects.filter(status=True)  #
+        return services
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        first = 1
+        service_paginate = Paginator(self.get_queryset(), 1)
+        last = service_paginate.num_pages
+        context["first"] = first
+        context["last"] = last
+        return context
+
+    def post(self, request, *args, **kwargs):
+        #       # برای خالی کردن سبد از متود زیر استفاده میشود
+        #       del request.session["cart"]
+        #       request.session.modified = True
+        # دیکشنری کارت از شژن فراخوانی میشود و اگر نباشد با یک دیکشنری خالی مقدار دهی میشود
+        cart = request.session.get("cart", {})
+        # آی دی محصود و تعدادش از رکوئست پست فراخوانی میشود
+        # علت استفاده از بلاک زی این است که ممکن است گاهی دیتای دیگری در رکوئست پست ارسال شود
+        # لذا برای اینکه سرور ارور ندهد با بلاک زیر بررسی میشود
+        try:
+            id = request.POST.get("product_id", "0")
+            quantity = request.POST.get("quantity", "1")
+        except ValueError:
+            return HttpResponseBadRequest("Invalid input")
+        # اگر آی دی محصول در سبد بود به مقدار قبلی اش اضافه میشود
+        # اگر نباشد آی دی به همراه مقدارش در کارت قرار میگیرد
+        if id in cart:
+            cart[id] = str(int(cart[id]) + int(quantity))
+        else:
+            cart[id] = quantity
+        # برای اطمینان بیشتر مجدد کارت در سژن ست میشود
+        request.session["cart"] = cart
+        # تغییرات سژن اعمال میشود
+        request.session.modified = True
+        return redirect(request.path_info)
 
 
 # def services(request, **kwargs):
